b89394e79d1e9fa0020ab994600a86710d9d79de
mawujn1234 <mawujun1234@163.com>
2016-03-17 20:20:33 +0800

    111

--- src/test/java/com/mawujun/generator/GeneratorM.java
+++ src/test/java/com/mawujun/generator/GeneratorM.java
@@ -1,9 +1,15 @@
+
 package com.mawujun.generator;
 
 import java.io.IOException;
 
 import com.mawujun.generator.MT.GeneratorMT;
 
+/**
+ * 生成M类。这个类的作用是用来在编码的时候引用的，而不需要写列名，这样在修改javabean中的列名后，就会自动修改修改
+ * @author mawujun 16064988@qq.com  
+ *
+ */
 public class GeneratorM {
 
 	public static void main(String[] args) throws IOException {
@@ -13,3 +19,5 @@ public class GeneratorM {
 	}
 
 }
+
+
